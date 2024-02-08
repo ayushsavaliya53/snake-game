c914acc3567a31b7d454f30b2bc0d1655b91bb50
ayushsavaliya53 <159151903+ayushsavaliya53@users.noreply.github.com>
2024-02-08 19:22:05 +0000

    all direction movement without turn, addition of cells

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let rtop = parseInt(Math.random() * 13) * 50
 let rleft = parseInt(Math.random() * 13) * 50
 let startUp, startDown, startRight, startLeft;
 let uppressed = false, downpressed = false, rightpressed = false, leftpressed = false
-
+let singleCell = true
 fruit.style.top = rtop + 'px'
 fruit.style.left = rleft + 'px'
 sBody[0].style.top = '0px'
@@ -16,7 +16,9 @@ sBody[0].style.left = '0px'
 document.body.addEventListener('keydown', function(eve) {
   switch (eve.key) {
     case "ArrowDown":
-      if (uppressed == false) {
+      if (!uppressed && !downpressed) {
+        clearInterval(startLeft)
+        clearInterval(startRight)
         leftpressed = false
         rightpressed = false
         downpressed = true
@@ -24,7 +26,9 @@ document.body.addEventListener('keydown', function(eve) {
       }
       break;
     case "ArrowUp":
-      if (downpressed == false) {
+      if (!uppressed && !downpressed) {
+        clearInterval(startLeft)
+        clearInterval(startRight)
         leftpressed = false
         rightpressed = false
         uppressed = true
@@ -32,7 +36,9 @@ document.body.addEventListener('keydown', function(eve) {
       }
       break;
     case "ArrowLeft":
-      if (rightpressed == false) {
+      if (!rightpressed && !leftpressed) {
+        clearInterval(startUp)
+        clearInterval(startDown)
         uppressed = false
         downpressed = false
         leftpressed = true
@@ -40,7 +46,9 @@ document.body.addEventListener('keydown', function(eve) {
       }
       break;
     case "ArrowRight":
-      if (leftpressed == false) {
+      if (!rightpressed && !leftpressed) {
+        clearInterval(startUp)
+        clearInterval(startDown)
         uppressed = false
         downpressed = false
         rightpressed = true
@@ -50,72 +58,150 @@ document.body.addEventListener('keydown', function(eve) {
   }
 })
 
-let moveDown = (lwrind, hrind) => {
+let moveDown = (lwrind = 0, hrind = len - 1) => {
 
   if (downpressed) {
     if (sBody[0].style.top.slice(0, -2) < 600) {
-      startDown = setTimeout(() => {
-        sBody[0].style.top = parseInt(sBody[0].style.top.slice(0, -2)) + 50 + 'px'
-        // if (eat()) {
-        //   sBody[1].style.top = parseInt(sBody[0].style.top.slice(0, -2)) - 50 + 'px'
-        //   sBody[1].style.left = sBody[0].style.left
-        // }
-        moveDown()
-      }, 500)
+      if (singleCell) {
+        startDown = setTimeout(() => {
+          sBody[0].style.top = parseInt(sBody[0].style.top.slice(0, -2)) + 50 + 'px'
+          if (eat()) {
+            sBody[0].style.top = parseInt(sBody[1].style.top.slice(0, -2)) + 50 + 'px'
+            sBody[0].style.left = sBody[1].style.left
+          }
+          moveDown()
+        }, 500)
+      }
+      else {
+        startDown = setTimeout(() => {
+          for (i = lwrind; i <= hrind; i++) {
+            sBody[i].style.top = parseInt(sBody[i].style.top.slice(0, -2)) + 50 + 'px'
+          }
+          if (eat()) {
+            sBody[0].style.top = parseInt(sBody[1].style.top.slice(0, -2)) + 50 + 'px'
+            sBody[0].style.left = sBody[1].style.left
+          }
+          // if (hrind < len - 1) {
+          //   moveDown(0, hrind + 1)
+          // } else {
+          //   moveDown(0, hrind)
+          // }
+          moveDown(0, len - 1)
+        }, 500)
+      }
     } else { downpressed = false }
 
   }
 
 }
-let moveUp = (lwrind, hrind) => {
+let moveUp = (lwrind = 0, hrind = len - 1) => {
   if (uppressed) {
     if (sBody[0].style.top.slice(0, -2) > 0) {
-      startUp = setTimeout(() => {
-        sBody[0].style.top = parseInt(sBody[0].style.top.slice(0, -2)) - 50 + 'px'
-        // if (eat()) {
-        //   sBody[1].style.top = parseInt(sBody[0].style.top.slice(0, -2)) + 50 + 'px'
-        //   sBody[1].style.left = sBody[0].style.left
-        // }
-        moveUp()
-      }, 500)
+      if (singleCell) {
+        startUp = setTimeout(() => {
+          sBody[0].style.top = parseInt(sBody[0].style.top.slice(0, -2)) - 50 + 'px'
+          if (eat()) {
+            sBody[0].style.top = parseInt(sBody[1].style.top.slice(0, -2)) - 50 + 'px'
+            sBody[0].style.left = sBody[1].style.left
+          }
+          moveUp()
+        }, 500)
+      }
+      else {
+        console.log(123)
+        startUp = setTimeout(() => {
+          for (i = lwrind; i <= hrind; i++) {
+            sBody[i].style.top = parseInt(sBody[i].style.top.slice(0, -2)) - 50 + 'px'
+          }
+          if (eat()) {
+            sBody[0].style.top = parseInt(sBody[1].style.top.slice(0, -2)) - 50 + 'px'
+            sBody[0].style.left = sBody[1].style.left
+          }
+          // if (hrind < len - 1) {
+          //   moveUp(0, hrind + 1)
+          // } else {
+          //   moveUp(0, hrind)
+          // }
+          moveUp(0, len - 1)
+        }, 500)
+      }
     } else { uppressed = false }
   }
 }
-let moveRight = (lwrind, hrind) => {
+let moveRight = (lwrind = 0, hrind = len - 1) => {
   if (rightpressed) {
     if (sBody[0].style.left.slice(0, -2) < 600) {
-      startRight = setTimeout(() => {
-        sBody[0].style.left = parseInt(sBody[0].style.left.slice(0, -2)) + 50 + 'px'
-        // if (eat()) {
-        //   sBody[1].style.left = parseInt(sBody[0].style.left.slice(0, -2)) - 50 + 'px'
-        //   sBody[1].style.top = sBody[0].style.top
-        // }
-        moveRight()
-      }, 500)
+      if (singleCell) {
+        startRight = setTimeout(() => {
+          sBody[0].style.left = parseInt(sBody[0].style.left.slice(0, -2)) + 50 + 'px'
+          if (eat()) {
+            sBody[0].style.left = parseInt(sBody[1].style.left.slice(0, -2)) + 50 + 'px'
+            sBody[0].style.top = sBody[1].style.top
+          }
+          moveRight()
+        }, 500)
+      }
+      else {
+        startRight = setTimeout(() => {
+          for (i = lwrind; i <= hrind; i++) {
+            sBody[i].style.left = parseInt(sBody[i].style.left.slice(0, -2)) + 50 + 'px'
+          }
+          if (eat()) {
+            sBody[0].style.left = parseInt(sBody[1].style.left.slice(0, -2)) + 50 + 'px'
+            sBody[0].style.top = sBody[1].style.top
+          }
+          // if (hrind < len - 1) {
+          //   moveRight(0, hrind + 1)
+          // } else {
+          //   moveRight(0, hrind)
+          // }
+          moveRight(0, len - 1)
+        }, 500)
+      }
     } else { rightpressed = false }
   }
 }
-let moveLeft = (lwrind, hrind) => {
+let moveLeft = (lwrind = 0, hrind = len - 1) => {
   if (leftpressed) {
     if (sBody[0].style.left.slice(0, -2) > 0) {
-      startLeft = setTimeout(() => {
-        sBody[0].style.left = parseInt(sBody[0].style.left.slice(0, -2)) - 50 + 'px'
-        // if (eat()) {
-        //   sBody[1].style.left = parseInt(sBody[0].style.left.slice(0, -2)) + 50 + 'px'
-        //   sBody[1].style.top = sBody[0].style.top
-        // }
-        moveLeft()
-      }, 500)
+      if (singleCell) {
+        startLeft = setTimeout(() => {
+          sBody[0].style.left = parseInt(sBody[0].style.left.slice(0, -2)) - 50 + 'px'
+          if (eat()) {
+            sBody[0].style.left = parseInt(sBody[1].style.left.slice(0, -2)) - 50 + 'px'
+            sBody[0].style.top = sBody[1].style.top
+          }
+          moveLeft()
+        }, 500)
+      }
+      else {
+        startLeft = setTimeout(() => {
+          for (i = lwrind; i <= hrind; i++) {
+            sBody[i].style.left = parseInt(sBody[i].style.left.slice(0, -2)) - 50 + 'px'
+          }
+          if (eat()) {
+            sBody[0].style.left = parseInt(sBody[1].style.left.slice(0, -2)) - 50 + 'px'
+            sBody[0].style.top = sBody[1].style.top
+          }
+          // if (hrind < len - 1) {
+          //   moveLeft(0, hrind + 1)
+          // } else {
+          //   moveLeft(0, hrind)
+          // }
+          moveLeft(0, len - 1)
+        }, 500)
+      }
     } else { leftpressed = false }
   }
 }
 
-// let eat = () => {
-//   if (sBody[0].style.top.slice(0, -2) == fruit.style.top.slice(0, -2) && sBody[0].style.left.slice(0, -2) == fruit.style.left.slice(0, -2)) {
-//     fruit.style.top = rtop + 'px'
-//     fruit.style.left = rleft + 'px'
-//     len += 1
-//     snake.insertBefore(sBody[0].cloneNode(true), sBody[0])
-//     return true
-//   }
-// }
+let eat = () => {
+  if (sBody[0].style.top.slice(0, -2) == fruit.style.top.slice(0, -2) && sBody[0].style.left.slice(0, -2) == fruit.style.left.slice(0, -2)) {
+    singleCell = false
+    len += 1
+    snake.insertBefore(sBody[0].cloneNode(true), snake.firstElementChild)
+    fruit.style.top = (parseInt(Math.random() * 13) * 50) + 'px'
+    fruit.style.left = (parseInt(Math.random() * 13) * 50) + 'px'
+    return true
+  }
+}
